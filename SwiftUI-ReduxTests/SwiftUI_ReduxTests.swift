8fe9630b642f010025734e403df97221a2b3ed98
kitasuke <yusuke2759@gmail.com>
2019-06-15 17:41:34 +0900

    Initial Commit

---
+++ SwiftUI-ReduxTests/SwiftUI_ReduxTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUI_ReduxTests.swift
+//  SwiftUI-ReduxTests
+//
+//  Created by Yusuke Kita on 6/15/19.
+//  Copyright © 2019 Yusuke Kita. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftUI_Redux
+
+class SwiftUI_ReduxTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
